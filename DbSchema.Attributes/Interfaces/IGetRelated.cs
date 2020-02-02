f286b7a547aad691a996519d8542a231b842b000
Adam O'Neil <adamosoftware@gmail.com>
2020-02-02 07:54:06 -0500

    version 1.0.1 added interfaces, along with new IValidate

---
+++ DbSchema.Attributes/Interfaces/IGetRelated.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace AO.DbSchema.Interfaces
+{
+    public interface IGetRelated<TModel>
+    {
+        /// <summary>
+        /// perform any additional Gets that are related to this model
+        /// </summary>        
+        Func<IDbConnection, TModel, Task> OnGetAsync { get; }
+    }
+}
---
+++ DbSchema.Attributes/Interfaces/IValidate.cs
@@ -0,0 +1,11 @@
+﻿using System.Data;
+using System.Threading.Tasks;
+
+namespace AO.DbSchema.Attributes.Interfaces
+{
+    public interface IValidate<TModel>
+    {
+        bool IsValid(TModel model);
+        Task<bool> IsValidAsync(IDbConnection connection, TModel model);
+    }
+}
