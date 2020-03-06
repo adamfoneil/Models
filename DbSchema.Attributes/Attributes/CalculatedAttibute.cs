f46e8d2f0f06802ad7ec34edd30dc081fc227724
Adam ONeil <adamosoftware@gmail.com>
2020-03-06 10:19:27 -0500

    version 1.0.8 Calculated attribute

---
+++ DbSchema.Attributes/Attributes/CalculatedAttibute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AO.DbSchema.Attributes.Attributes
+{
+    /// <summary>
+    /// denotes a calculated column
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class CalculatedAttibute : Attribute
+    {
+        public CalculatedAttibute(string expression, bool persisted = false)
+        {
+            Expression = expression;
+            IsPersisted = persisted;
+        }
+
+        public string Expression { get; }
+        public bool IsPersisted { get; }
+    }
+}
