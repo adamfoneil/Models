7d8027a825c1894c4bfb87a7e810c7ad980651da
Adam ONeil <adamosoftware@gmail.com>
2020-04-22 17:29:22 -0400

    version 1.0.13 added ITextLookup

---
+++ DbSchema.Attributes/Interfaces/ITextLookup.cs
@@ -0,0 +1,14 @@
+﻿using System.Data;
+using System.Threading.Tasks;
+
+namespace AO.DbSchema.Attributes.Interfaces
+{
+    /// <summary>
+    /// Use this on model classes to convert key values to text equivalents.
+    /// Used with logged change tracking in Dapper.CX.ChangeTracking
+    /// </summary>
+    public interface ITextLookup
+    {
+        Task<string> GetTextFromKeyAsync(IDbConnection connection, string propertyName, object keyValue);
+    }
+}
