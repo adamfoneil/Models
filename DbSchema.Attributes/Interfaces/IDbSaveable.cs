82154ce7d1b6c144de0ec6dc932365bff5391f4a
Adam O'Neil <adamosoftware@gmail.com>
2020-04-25 11:00:51 -0400

    version 1.0.16 added IDbSaveable

---
+++ DbSchema.Attributes/Interfaces/IDbSaveable.cs
@@ -0,0 +1,13 @@
+﻿using System.Data;
+using System.Threading.Tasks;
+
+namespace AO.DbSchema.Attributes.Interfaces
+{
+    /// <summary>
+    /// use this to make anything able to be saved in a database.    
+    /// </summary>
+    public interface IDbSaveable
+    {
+        Task SaveAsync(IDbConnection connection);
+    }
+}
