189adae6a470c9b442782505142d3dfd3ad7d503
Adam ONeil <adamosoftware@gmail.com>
2020-05-17 16:15:41 -0400

    removed unnecessary method

--- DbSchema.Attributes/Interfaces/ITenantIsolated.cs
+++ DbSchema.Attributes/Interfaces/ITenantIsolated.cs
@@ -12,10 +12,5 @@ namespace AO.Models.Interfaces
         /// this can be an ordinary property value, but can also be a database query through a join
         /// </summary>
         Task<T> GetTenantIdAsync(IDbConnection connection, IDbTransaction txn = null);
-
-        /// <summary>
-        /// called whenever Crud actions are performed on a model to verify that the user has permission
-        /// </summary>
-        Task<bool> IsValidTenantAsync(IDbConnection connection, IUser<T> user, IDbTransaction txn = null);
     }
 }
