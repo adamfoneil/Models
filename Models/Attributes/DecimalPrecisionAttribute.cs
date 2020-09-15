c1d966b17085c1ddcb4e15c9dc43a8adc8187727
Adam O'Neil <adamosoftware@gmail.com>
2020-09-15 16:01:34 -0400

    version 1.1.5 added DecimalPrecision attribute

---
+++ Models/Attributes/DecimalPrecisionAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AO.Models.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class DecimalPrecisionAttribute : Attribute
+    {
+        public DecimalPrecisionAttribute(byte precision, byte scale)
+        {
+            if (Precision < Scale) throw new ArgumentException("Precision must be equal or greater than scale.");
+
+            Precision = precision;
+            Scale = scale;
+        }
+
+        public byte Scale { get; }
+        public byte Precision { get; }
+    }
+}
