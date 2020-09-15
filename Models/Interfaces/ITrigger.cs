f9fc82fdbc3697d7dec62098cd0622c560e16617
Adam O'Neil <adamosoftware@gmail.com>
2020-09-15 11:21:26 -0400

    version 1.1.4 added IUserBase args to ITrigger

--- Models/Interfaces/ITrigger.cs
+++ Models/Interfaces/ITrigger.cs
@@ -5,12 +5,13 @@ using System.Threading.Tasks;
 namespace AO.Models.Interfaces
 {
     /// <summary>
-    /// use much as you would SQL Server triggers, but recommend limit actions to be database-specific.
-    /// For example, don't send email nor call external services
+    /// use much as you would SQL Server triggers, but recommend limit actions to be database-internal.
+    /// For example, don't send email nor call external services because of the depdendencies you'd have to add
     /// </summary>
     public interface ITrigger
     {
-        Task RowSavedAsync(IDbConnection connection, SaveAction saveAction, IDbTransaction txn = null);
-        Task RowDeletedAsync(IDbConnection connection, IDbTransaction txn = null);
+        Task RowSavingAsync(IDbConnection connection, SaveAction saveAction, IDbTransaction txn = null, IUserBase user = null);
+        Task RowSavedAsync(IDbConnection connection, SaveAction saveAction, IDbTransaction txn = null, IUserBase user = null);
+        Task RowDeletedAsync(IDbConnection connection, IDbTransaction txn = null, IUserBase user = null);
     }
 }
