b6de10b983d6b812e458df3a5803ab1c3eeaeca4
Adam O'Neil <adamosoftware@gmail.com>
2020-09-17 14:14:03 -0400

    version 1.1.8 added TrackChanges attribute

---
+++ Models/Attributes/TrackChangesAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AO.Models.Attributes
+{
+    /// <summary>
+    /// use this to turn on logged changed tracking on a model class
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public class TrackChangesAttribute : Attribute
+    {
+        public TrackChangesAttribute(params string[] ignoreProperties)
+        {
+            IgnoreProperties = ignoreProperties;
+        }
+
+        public IEnumerable<string> IgnoreProperties { get; }
+    }
+}
