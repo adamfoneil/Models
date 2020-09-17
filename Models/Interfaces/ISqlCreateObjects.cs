5a596c321b92f5699ef8b56a51678f72fbadde1c
Adam O'Neil <adamosoftware@gmail.com>
2020-09-17 10:33:23 -0400

    version 1.1.6 added ISqlCreateObjects

---
+++ Models/Interfaces/ISqlCreateObjects.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AO.Models.Interfaces
+{
+    /// <summary>
+    /// describes an object that provides SQL statements
+    /// </summary>
+    public interface ISqlCreateObjects
+    {
+        IEnumerable<string> GetStatements(IEnumerable<Type> types);
+    }
+}
