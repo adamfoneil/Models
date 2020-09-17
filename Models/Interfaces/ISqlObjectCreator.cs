d8029acf5cf0879de73cbffa53e6b3ae9ad66a7f
Adam O'Neil <adamosoftware@gmail.com>
2020-09-17 10:43:34 -0400

    version 1.1.7 added  ISqlObjectCreator

---
+++ Models/Interfaces/ISqlObjectCreator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace AO.Models.Interfaces
+{
+    /// <summary>
+    /// describes an object that provides SQL statements that create objects
+    /// </summary>
+    public interface ISqlObjectCreator
+    {
+        Task<IEnumerable<string>> GetStatementsAsync(IDbConnection connection, IEnumerable<Type> types);
+    }
+}
