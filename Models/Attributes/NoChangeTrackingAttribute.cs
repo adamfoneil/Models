1b6c9ab537ba636bb9dfbdac33b48ffdd32e4519
Adam O'Neil <adamosoftware@gmail.com>
2020-09-24 11:47:37 -0400

    version 1.1.13 added [NoChangeTracking] attribute

---
+++ Models/Attributes/NoChangeTrackingAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AO.Models.Attributes
+{
+    /// <summary>
+    /// causes a property to be ignored by change tracking
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class NoChangeTrackingAttribute : Attribute
+    {
+    }
+}
