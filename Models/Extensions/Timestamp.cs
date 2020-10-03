992312f73a5226cd2b7a070e9b7b805d0835def1
Adam O'Neil <adamosoftware@gmail.com>
2020-10-03 10:38:47 -0400

    oops I forgot the actual method

--- Models/Extensions/Timestamp.cs
+++ Models/Extensions/Timestamp.cs
@@ -1,10 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace AO.Models.Extensions
 {
     public static class Timestamp
     {
+        public static DateTime Local(string timeZoneId)
+        {
+            try
+            {
+                var tz = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
+                return TimeZoneInfo.ConvertTime(DateTime.UtcNow, tz);
+            }
+            catch
+            {
+                return DateTime.UtcNow;
+            }
+        }
     }
 }
