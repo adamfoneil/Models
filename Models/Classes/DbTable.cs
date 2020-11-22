ea966d8c74a404d59f336a98e7c4dfb1aa57775d
Adam O'Neil <adamosoftware@gmail.com>
2020-11-22 09:54:38 -0500

    added DbTable

---
+++ Models/Classes/DbTable.cs
@@ -0,0 +1,11 @@
+﻿namespace AO.Models.Classes
+{
+    /// <summary>
+    /// extension point for table-level behaviors (stamping, validation, triggers) that apply to a model class.
+    /// This would be passed to Dapper.CX methods as optional arg. Implement existing interfaces here as you want:
+    /// IAudit, ITrigger, IGetRelated, IValidate, etc. This moves table-level, business layer stuff out of model layer
+    /// </summary>    
+    public class DbTable<TModel>
+    {
+    }
+}
