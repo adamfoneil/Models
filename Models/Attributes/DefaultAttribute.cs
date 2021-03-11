7c2789ce2541da7993f4a4f3095932b93a045496
Adam O'Neil <adamosoftware@gmail.com>
2021-03-11 09:55:47 -0500

    v1.1.29 added Default attribute

---
+++ Models/Attributes/DefaultAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AO.Models.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class DefaultAttribute : Attribute
+    {
+        public DefaultAttribute(string expression)
+        {
+            Expression = expression;
+        }
+
+        public string Expression { get; }
+    }
+}
