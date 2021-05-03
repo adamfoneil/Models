531496160d441f2a7c906259e624a9eb132db3a0
Adam Oneil <adamsoftware@gmail.com>
2021-05-03 11:11:58 -0400

    v1.1.32 added IRepository

---
+++ Models/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace AO.Models.Interfaces
+{
+    public interface IRepository<TModel, TKey, TUser>
+        where TModel : IModel<TKey>
+        where TUser : IUserBase
+    {
+        Task<TModel> GetAsync(TUser user, TKey id);
+        Task<TModel> SaveAsync(TUser user, TModel model);
+        Task DeleteAsync(TUser user, TKey id);
+    }
+}
