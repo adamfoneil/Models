73f770ccfb77ac6a0f45931a8981b358e3343c43
Adam Oneil <adamsoftware@gmail.com>
2021-06-05 20:02:18 -0400

    v1.1.38 added CheckConstraint attribute

---
+++ Models/Attributes/CheckConstraintAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AO.Models.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    public class CheckConstraintAttribute : Attribute
+    {
+        public CheckConstraintAttribute(string constraintName, string expression)
+        {
+            ConstraintName = constraintName;
+            Expression = expression;
+        }
+
+        public string ConstraintName { get; }
+        public string Expression { get; }
+    }
+}
